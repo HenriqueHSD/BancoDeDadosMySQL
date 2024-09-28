0eebd6035558ad1de50233754c41687caa9fa605
henrique <henriquedamiao1709@gmail.com>
2024-09-28 02:58:50 -0300

    class Department create

---
+++ JavaBanco/src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department ob = new Department(1, "Henrique");
+		
+		System.out.println(ob);
+
+	}
+
+}
