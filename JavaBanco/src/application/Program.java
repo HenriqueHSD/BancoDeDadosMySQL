4906593a6511ab9c36013197c018f9e843d80316
henrique <henriquedamiao1709@gmail.com>
2024-10-01 21:58:32 -0300

    findById implementation

--- JavaBanco/src/application/Program.java
+++ JavaBanco/src/application/Program.java
@@ -1,24 +1,19 @@
 package application;
 
-import java.time.LocalDate;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department ob = new Department(1, "Henrique");
-		
-		Seller seller = new Seller(1, "damiao", "teste@teste", LocalDate.now(), 2500.00, ob);
-		
-		System.out.println(ob);
-		System.out.println(seller);
 		
 		SellerDao sell = DaoFactory.createSellerDao();
+		
+		Seller sel = sell.findById(3);
+		
+		System.out.println(sel);
 
 	}
 
