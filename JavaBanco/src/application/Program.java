870a698b8566007d4b449dff3c1c11b8fa62ff0b
henrique <henriquedamiao1709@gmail.com>
2024-10-02 20:33:38 -0300

    implementando findByDepartment

--- JavaBanco/src/application/Program.java
+++ JavaBanco/src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -11,9 +14,22 @@ public class Program {
 		
 		SellerDao sell = DaoFactory.createSellerDao();
 		
+		System.out.println("== teste FindById ==");
+		
 		Seller sel = sell.findById(3);
 		
 		System.out.println(sel);
+		
+		System.out.println("\n== teste FindByDepartment ==");
+		Department department = new Department(2, null);
+		
+		List<Seller> list = sell.findByDepartment(department);
+		
+		for(Seller li : list) {
+			System.out.println(li);
+		}
+		
+		
 
 	}
 
